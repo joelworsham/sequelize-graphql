849d839283708c783e10646611685a332bb36571
joelworsham <joelworsham@gmail.com>
2021-03-14 13:46:45 -0400

    fix reference error

--- src/util/graphql/createArgsFromDbModel.js
+++ src/util/graphql/createArgsFromDbModel.js
@@ -97,7 +97,6 @@ const createArgsFromDbModel = (
                 fields: extraFields,
                 exclude,
                 require,
-                pagination,
               },
             ),
           });
